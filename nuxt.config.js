e151fcb60c143c705983c987fb8cefd01f8b6495
ChKhasan <chulliyevhasan1499@mail.ru>
2023-06-08 14:51:23 +0500

    first commit 4

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,12 @@ export default {
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
-  css: ["@/assets/css/app.css", "ant-design-vue/dist/antd.css"],
+  css: [
+    "@/assets/css/app.css",
+    "ant-design-vue/dist/antd.css",
+    "https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&family=Roboto+Serif:ital,wght@0,200;1,200&display=swap",
+    "https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter:wght@400;500;600;700&family=Lato:wght@300;400;700;900&family=Mulish:wght@200;300;400;500;600;700;800&family=Roboto+Serif:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap",
+  ],
 
   plugins: [
     { src: "~plugins/antdv.js" },
