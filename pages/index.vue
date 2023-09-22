6d03867b39cbf9b56f3d149b090276617908feae
Khasan_Ch <chulliyevhasan1405@mail.ru>
2023-09-22 18:49:30 +0500

    swiper

--- pages/index.vue
+++ pages/index.vue
@@ -744,7 +744,7 @@ export default {
   width: 100% !important;
 }
 .swiper-pagination {
-  position: static;
+  position: static !important;
   display: flex;
   align-items: center;
   margin-top: 10px;
